09bdab05d6dcc356bc206c97503f95894bb28e3a
DJ Meyers <djmeyers@outlook.com>
2022-11-08 00:30:47 -0600

    Favicon and styles

--- ClientApp/src/layout/PageBase.tsx
+++ ClientApp/src/layout/PageBase.tsx
@@ -5,7 +5,7 @@ import { Header } from './Header';
 export const PageBase: React.FC<{ children: ReactNode[] }> =({ children }) => {
 
     return (
-        <div className='flex flex-col h-full'>
+        <div className='flex flex-col h-screen bg-slate-200'>
             <Header />
             <div className='grow'>
                 {children}
